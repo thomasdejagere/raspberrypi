b840ad411f565a209aa0d4e1c36838f9302f050b
Thomas Dejagere <thomasdejagere@gmail.com>
2017-02-23 15:43:53 +0100

    Update app.js
    init raspberrypi commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-//dit is een test om te kijken of de sync goed werkt
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost/todo-api')
