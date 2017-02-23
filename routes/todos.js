966f3d424264406bcf32b367e1804f7001b3eb04
Thomas Dejagere <thomasdejagere@gmail.com>
2017-02-23 17:56:16 +0100

    added get todos route

---
+++ routes/todos.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Todo = require('../models/Todo.js');
+
+/* GET /todos listing. */
+router.get('/', function(req, res, next) {
+  Todo.find(function (err, todos) {
+    if (err) return next(err);
+    res.json(todos);
+  });
+});
+
+/* POST /todos */
+router.post('/', function(req, res, next) {
+  Todo.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
